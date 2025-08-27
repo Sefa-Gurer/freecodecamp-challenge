6ec2aa825ba9c2692672aa584aa7c00fd24426dd
Sefa-Gurer <sgurer3@gmail.com>
2025-08-27 17:33:36 +0300

    Create 26.08.2025.py

---
+++ 26.08.2025.py
@@ -0,0 +1,26 @@
+def decode(s):
+    def find_innermost_arr(arr):
+        innermost_left_paren_index = arr.rfind('(')
+        if innermost_left_paren_index == -1:
+            return ""
+
+        innermost_right_paren_index = arr.find(')', innermost_left_paren_index)
+
+        if innermost_right_paren_index == -1:
+            return ""
+        
+        if arr[innermost_left_paren_index] == "(":
+            innermost_arr=arr[(innermost_left_paren_index+1):(innermost_right_paren_index)]
+
+            innermost_arr = list(innermost_arr)
+            innermost_arr.reverse()
+
+            arr = arr[0:innermost_left_paren_index]+"".join(innermost_arr)+arr[(innermost_right_paren_index+1):]
+        return arr
+    
+    while "(" in s:
+        s = find_innermost_arr(s)
+    return s
+
+result = decode("f(Ce(re))o((e(aC)m)d)p")
+print(result)
